c5f1f7eefaa6fdbef87c00bf15855527cb332f56
Flooooooooooorian <florian.weber@neuefische.de>
2024-09-24 14:57:40 +0200

    Remove unused variables

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,18 +4,10 @@ public class Main {
     public static void main(String[] args) {
 
         int age1 = 51;
-        String name1 = "Florian";
-
-        int age2 = 12;
-        String name2 = "Martin";
-
-        int age3 = 36;
-        String name3 = "Dominic";
 
         boolean result = checkAge(age1);
         System.out.println(result);
 
-        System.out.println(checkAge(age2));
         checkAge(96);
         checkAge(0);
         checkAge(-46);
