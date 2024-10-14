54d9a144ac5358faa3900337979953b57499d19a
Flooooooooooorian <florian.weber@neuefische.de>
2024-10-14 14:21:23 +0200

    Add arrayList

---
+++ src/main/java/git/Main.java
@@ -0,0 +1,10 @@
+package git;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+    }
+}
